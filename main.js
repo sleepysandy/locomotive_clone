9bc69a3e7591477165de44efd60c887927748085
sandyshen <sandyshen@rytass.com>
2021-08-15 21:00:36 +0800

    feat(outline-section):layout v1

--- main.js
+++ main.js
@@ -1,23 +1,95 @@
 gsap.registerPlugin(ScrollTrigger);
 
+
+
+ gsap.fromTo(document.querySelector('#key-visual-section header'), {
+    y: 0,
+  }, {
+    y: '-=200px',
+    scrollTrigger: {
+      start: "top top",
+      end: "30%",
+      scrub: true,
+    },
+  });
+
+  gsap.fromTo(document.querySelector('#key-visual-section .left-title'), {
+    x: 0,
+    y: 0,
+  }, {
+    x: '+=200px',
+    scrollTrigger: {
+      start: "top top",
+      end: "60%",
+      scrub: true,
+    },
+  });
+  
+  gsap.fromTo(document.querySelector('#key-visual-section .right-title'), {
+    x: 0,
+    y: 0,
+  }, {
+    x: '-=200px',
+    scrollTrigger: {
+      start: "top top",
+      end: "60%",
+      scrub: true,
+    },
+  });
+  
+  gsap.fromTo(document.querySelector('#key-visual-section .first-line'), {
+    transform: 'translateY(0)',
+  }, {
+    transform: 'translateY(-20vh)',
+    scrollTrigger: {
+      start: 'top top',
+      end: '50%',
+      scrub: true,
+    },
+  });
+ 
+  gsap.fromTo(document.querySelector('#key-visual-section .second-line'), {
+    transform: 'translateY(0)',
+  }, {
+    transform: 'translateY(-15vh)',
+    scrollTrigger: {
+      start: 'top top',
+      end: '50%',
+      scrub: true,
+    },
+  });
+
 const splitTextElements = document.querySelectorAll('#key-visual-section .split-version > span');
 
 gsap.to(Array.from(splitTextElements), {
     y: (index) => {
         switch (index) {
             case 0:
-                return '-=15vh';
+                return '-=36vh';
             case 1:
-                return '-=30vh';
+                return '-=50vh';
             case 2:
-                return '-=5vh';
+                return '-=15vh';
             case 3:
-                return '-=23vh';
+                return '-=42vh';
         }
     },
     scrollTrigger:{
         start: 'top top',
-        end: 'bottom bottom',
+        end: '50%',
         scrub: true,
     },
 });
+
+
+gsap.fromTo(document.querySelector('#key-visual-section .logo'), {
+    transform: 'translateY(0)',
+  }, {
+    transform: 'translateY(-10vh)',
+    scrollTrigger: {
+      start: 'top top',
+      end: '50%',
+      scrub: true,
+    },
+  });
+
